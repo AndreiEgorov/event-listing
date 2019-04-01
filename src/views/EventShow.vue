731a5c5567d124233e7318626296d4b2352c93f8
Andrei Egorov <andreiegorov@Andreis-MacBook-Pro-2.local>
2019-03-31 19:36:13 -0700

    Add event calling method to event service, style event card

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,12 +1,66 @@
+nformation. Now here’s EventShow.vue:
+
 <template>
     <div>
-        <h1>Event Show # {{id}}</h1>
+        <div class="event-header">
+            <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+            <h1 class="title">{{ event.title }}</h1>
+            <h5>Organized by {{ event.organizer }}</h5>
+            <h5>Category: {{ event.category }}</h5>
+        </div>
+        <Icon name="map"><h2>Location</h2></Icon>
+        <address>{{ event.location }}</address>
+        <h2>Event details</h2>
+        <p>{{ event.description }}</p>
+        <h2>Attendees
+            <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
+        </h2>
+        <ul class="list-group">
+            <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
+                <b>{{ attendee.name }}</b>
+            </li>
+        </ul>
     </div>
-
 </template>
-
 <script>
+    import eventService from  '@/services/eventServices.js'
     export default {
-        props: ["id"]
+        props: ["id"],
+        data(){
+            return{
+                event:{}
+            }
+
+        },
+        created(){
+            eventService.getEvent(this.id)
+                .then(response =>{
+                    this.event = response.data
+                })
+                .catch(e =>{
+                    console.log("There was an error", e.response)
+                })
+        }
     }
 </script>
+
+<style scoped>
+    .location {
+        margin-bottom: 0;
+    }
+    .location > .icon {
+        margin-left: 10px;
+    }
+    .event-header > .title {
+        margin: 0;
+    }
+    .list-group {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    .list-group > .list-item {
+        padding: 1em 0;
+        border-bottom: solid 1px #e5e5e5;
+    }
+</style>
